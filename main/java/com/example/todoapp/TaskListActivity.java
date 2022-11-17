ef0b89d0033cb2c123d0e9e772c83b765aed5751
Fipek <filip.bialowas2504@gmail.com>
2022-11-17 23:30:12 +0100

    xd

--- main/java/com/example/todoapp/TaskListActivity.java
+++
@@ -1,9 +0,0 @@
-package com.example.todoapp;
-import androidx.fragment.app.Fragment;
-
-public class TaskListActivity extends SingleFragmentActivity {
-    @Override
-    protected Fragment createFragment() {
-        return new TaskListFragment();
-    }
-}
